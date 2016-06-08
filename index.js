0cd8ba7b8c8bed7edfc816136b88f9e172b2c7cc
Mula Gilbert Njira <mula@isys.co.ke>
2016-06-08 13:27:24 +0300

    Calculator cli.

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+#! /usr/bin/env node
+
+var readline = require('readline');
 var Evaluator = require('./evaluator.js');
-var input = process.argv.slice(2).join(' ');
-// input = '15*8/(98 - 99)';
-
-console.log(input);
-
+// var input = process.argv.slice(2).join(' ');
+// // input = '15*8/(98 - 99)';
 
 var evaluator = new Evaluator();
-console.log(evaluator.evaluate(input));
 
+var reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.setPrompt('calc > ');
+reader.prompt();
+
+reader.on('line', (line) => {
+  switch (line.trim()) {
+    case 'q':
+    case 'Q':
+      reader.close();
+      return;
+    default:
+      try {
+        console.log(evaluator.evaluate(line));
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+  }
+
+  reader.prompt();
+}).on('close', () => {
+  console.log('Thank you for using the calculator.');
+  process.exit(0);
+});
